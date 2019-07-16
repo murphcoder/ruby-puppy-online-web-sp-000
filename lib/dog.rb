c1242db977eb856392bb83a0a02fcc634b0a45ae
Sean Murphy <filmmakersean@fastmail.net>
2019-07-16 14:56:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,18 @@
 # Add your code here
 
+class Dog
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def self.all
+    @@all
+  end
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+end
